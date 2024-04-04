a34a63ce43b8a6e29075cc887b20f1d821b3b837
Richard Seal <2706339+GuitarRich@users.noreply.github.com>
2024-04-04 14:49:35 -0400

    Svg image (#1)
    * Initial test for SVG inline rendering
    
    * inline SVGs working
    
    * Add 2nd exiting host
    
    * Fix linting error
    
    * More linting fixes

--- src/sxastarter/src/components/Image.tsx
+++ src/sxastarter/src/components/Image.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Image as JssImage,
   Link as JssLink,
@@ -27,6 +27,53 @@ const ImageDefault = (props: ImageProps): JSX.Element => (
     </div>
   </div>
 );
+
+const NotSVGImage = (props: ImageProps, pageState: string): JSX.Element => {
+  const Image = () => <JssImage field={props.fields.Image} />;
+  const id = props.params.RenderingIdentifier;
+
+  return (
+    <div className={`component image ${props.params.styles}`} id={id ? id : undefined}>
+      <div className="component-content">
+        {pageState === 'edit' || !props.fields.TargetUrl?.value?.href ? (
+          <Image />
+        ) : (
+          <JssLink field={props.fields.TargetUrl}>
+            <Image />
+          </JssLink>
+        )}
+        <Text
+          tag="span"
+          className="image-caption field-imagecaption"
+          field={props.fields.ImageCaption}
+        />
+      </div>
+    </div>
+  );
+};
+
+const SVGImage = (props: ImageProps): JSX.Element => {
+  const svgPath = props.fields?.Image.value?.src ? props.fields?.Image.value?.src : '';
+
+  const [svgData, setSVGData] = useState({});
+
+  useEffect(() => {
+    if (svgPath) {
+      const path = './-/media/' + svgPath.split('-/media')[1];
+      console.log('svgPath: ', path);
+      const data = async () => {
+        const Data = (await import('axios')).default;
+        Data.get(path).then((response) => {
+          const svg = response.data;
+          setSVGData(svg);
+        });
+      };
+      data();
+    }
+  }, [svgPath]); // Add closing parenthesis and semicolon here
+
+  return <div dangerouslySetInnerHTML={{ __html: svgData }} />;
+};
 
 export const Banner = (props: ImageProps): JSX.Element => {
   const { sitecoreContext } = useSitecoreContext();
@@ -60,27 +107,23 @@ export const Default = (props: ImageProps): JSX.Element => {
   const { sitecoreContext } = useSitecoreContext();
 
   if (props.fields) {
-    const Image = () => <JssImage field={props.fields.Image} />;
-    const id = props.params.RenderingIdentifier;
+    if (sitecoreContext.pageEditing) {
+      return <NotSVGImage {...props} />;
+    } else {
+      if (props.fields?.Image.value?.src?.includes('.svg')) {
+        return (
+          <>
+            Test SVG Image
+            <br />
+            <div style={{ border: '1px solid red', display: 'block', width: '100%' }}>
+              <SVGImage {...props} />
+            </div>
+          </>
+        );
+      }
 
-    return (
-      <div className={`component image ${props.params.styles}`} id={id ? id : undefined}>
-        <div className="component-content">
-          {sitecoreContext.pageState === 'edit' || !props.fields.TargetUrl?.value?.href ? (
-            <Image />
-          ) : (
-            <JssLink field={props.fields.TargetUrl}>
-              <Image />
-            </JssLink>
-          )}
-          <Text
-            tag="span"
-            className="image-caption field-imagecaption"
-            field={props.fields.ImageCaption}
-          />
-        </div>
-      </div>
-    );
+      return <NotSVGImage {...props} />;
+    }
   }
 
   return <ImageDefault {...props} />;
--- src/sxastarter/src/components/Promo.tsx
+++ src/sxastarter/src/components/Promo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Image as JssImage,
   Link as JssLink,
@@ -28,13 +28,46 @@ const PromoDefaultComponent = (props: PromoProps): JSX.Element => (
   </div>
 );
 
+const TabItem = (props: { tabHeading: string }): JSX.Element => {
+  useEffect(() => {
+    console.log('TabItem component mounted', props.tabHeading);
+    const event = new CustomEvent('onTabLoaded', { detail: { tabHeading: props.tabHeading } });
+    document?.dispatchEvent(event);
+  }, [props.tabHeading]);
+
+  return (
+    <div className="tab-item">
+      <span>{props.tabHeading}</span>
+    </div>
+  );
+};
+
 export const Default = (props: PromoProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
+  const [activeTabPath, setActiveTabPath] = React.useState<string>('');
+  const [tabHeadings, setTabHeadings] = React.useState<string[]>([]);
+
+  useEffect(() => {
+    console.log('Promo component mounted', props.params.RenderingId);
+    document?.addEventListener('onTabLoaded', (e: CustomEvent) => {
+      console.log('onTabLoaded event received', e.detail.tabHeading);
+      const tabHeading = e.detail.tabHeading;
+      setTabHeadings((prevTabHeadings) => [...prevTabHeadings, tabHeading]);
+    });
+  }, [props.params.RenderingId]);
+
+  useEffect(() => {
+    console.log('Promo component updated');
+    setActiveTabPath('1');
+  }, [activeTabPath, tabHeadings]);
+
   if (props.fields) {
     return (
       <div className={`component promo ${props.params.styles}`} id={id ? id : undefined}>
         <div className="component-content">
           <div className="field-promoicon">
+            <TabItem tabHeading="Tab 1" />
+            <TabItem tabHeading="Tab 2" />
             <JssImage field={props.fields.PromoIcon} />
           </div>
           <div className="promo-text">
@@ -57,6 +90,7 @@ export const Default = (props: PromoProps): JSX.Element => {
 
 export const WithText = (props: PromoProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
+
   if (props.fields) {
     return (
       <div className={`component promo ${props.params.styles}`} id={id ? id : undefined}>
